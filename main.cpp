bbc41f6528eda35ba2a58fe72b25052addae26f7
Perfect_03 <zlobin_gg52926@mail.ru>
2022-02-22 22:06:46 +0600

    finished version 1.1

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 int main(int argc, char *argv[])
 {
    QCoreApplication a(argc, argv);
+   //не знаю, нужно это или нет, но ниже проверка использования функций
     Stack<int> t;
     t.Push(5);
     t.Push(4);
-    t.Push(2);
-    //t.Print();
-    cout << 5;
+    t.Push(13);
+    t.Print();
     return t.Pop();
 }
