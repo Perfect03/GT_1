217e1f3192710ac6fe6c76b4e05976946e8e8686
Perfect_03 <zlobin_gg52926@mail.ru>
2022-02-22 22:03:24 +0600

    finished version 1.0

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <QCoreApplication>
+#include <iostream>
 #include "stack.h"
 
 int main(int argc, char *argv[])
 {
    QCoreApplication a(argc, argv);
-
-    return 1;// a.exec();
+    Stack<int> t;
+    t.Push(5);
+    t.Push(4);
+    t.Push(2);
+    //t.Print();
+    cout << 5;
+    return t.Pop();
 }
--- stack.h
+++ stack.h
@@ -1,21 +1,21 @@
+
 #include <iostream>
 #include <string>
 #include <functional>
+#include <list>
 
 #include "e_stack_exception.h"
 #include "e_stack_empty.h"
  
 using namespace std;
 
-#define SIZE 5
-
 template <typename T> class Stack {
    public:
-
+   Stack(int = 5); // конструктор по умолчанию, по умолчанию стек содержит 5 узлов
    ~Stack();
    void Push(const T &a); //Помещение объекта в стек;
+   void Print(); // Вывод элементов стека
    const T Pop(); // Извлечение объекта из стека;
-   int Size(); // Получение размерности стека.
 
    private:
    
@@ -25,14 +25,21 @@ template <typename T> class Stack {
 
 		Node *prev_ = nullptr; // указатель на предыдущий элемент
 	};
+   T *stackPtr; // указатель на стек
    Node *back_ = nullptr; // последний элемент стека
-   int sz = 0; // изначальная размерность
+   int size = 0; // изначальная размерность
 
 };
 
+template <typename T> Stack<T>::Stack(int maxSize) : size(maxSize) // конструктор
+{
+    stackPtr = new T[size];
+    back_ = 0; // инициализируем текущий элемент нулем
+}
+
 template <class T> Stack<T>::~Stack()
 {
-    //Clear();
+    delete [] stackPtr;
 }
 
 template <class T> void Stack<T>::Push(const T &a)
@@ -46,5 +53,42 @@ template <class T> void Stack<T>::Push(const T &a)
 
 	back_ = node; // делаем только что созданное звено последним
 
-    sz++; // размерность стека
+    size++; // размерность стека
+}
+
+template <class T> void Stack<T>::Print()
+{
+    if (back_ == nullptr) // если стек пуст - ошибка
+    {
+        throw EStackEmpty();
+    }
+
+            else {
+        Node* q=back_;
+                while (q != nullptr)
+                {
+                    cout << q->b << ' ';
+                    q = q->prev_;
+                }
+            }
+}
+
+template <class T> const T Stack<T>::Pop()
+{
+    if (back_ == nullptr) // если стек пуст - ошибка
+    {
+        throw EStackEmpty();
+    }
+
+    const T value = back_->b; // сохраняем значение узла
+
+    Node *temp = back_; // сохраняем ссылку на узел
+
+    back_ = back_->prev_; // перемещаем конец стека
+
+    delete temp; // удаляем узел
+
+    size--;
+
+    return value; // возвращаем сохраненое значение звена
 }
