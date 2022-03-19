bc63ad79a13ed59326b295fe23402857c54d558c
Perfect_03 <zlobin_gg52926@mail.ru>
2022-03-19 23:04:49 +0600

    add "writePersons" function

--- person_keeper.cpp
+++ person_keeper.cpp
@@ -25,7 +25,21 @@ void PersonKeeper::ReadPersons(QString path)
 
 void PersonKeeper::WritePersons(QString path) const
 {
-	
+	QFile file(path);
+
+
+	if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) //если файл недоступен для записи
+	{
+		throw "Couldn't open a file"; //выбрасываем исключение
+	}
+
+	QTextStream stream(&file); //поток текстовых данных файла
+
+	stack_.ForEach([&](const Person &value) // перебираем все имена
+	{
+		stream << value.last_name() << '\t' << value.first_name() << '\t' << value.patronymic() << Qt::endl; // записываем
+	});
+	file.close();
 }
 
 
