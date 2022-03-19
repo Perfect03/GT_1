876b8c0c0c7395c6f235ca177d31f08810d05763
Perfect_03 <zlobin_gg52926@mail.ru>
2022-03-19 12:41:36 +0600

    add "persons.txt" and "person.h v2"

--- person.h
+++ person.h
@@ -10,13 +10,19 @@ class Person
 {
 private:
 	long key;
-	std::string last_name;
-	std::string first_name;
+	std::string last_name_;
+	std::string first_name_;
+	std::string patronymic_;
 public:
 	Person();
 	Person(const Person&);
-	const std::string& getLastName() const { return last_name; }
-	const std::string& getFirstName() const { return first_name; }
-	void setLastName(const std::string&);	
-	void setFirstame(const std::string&);
+
+	const std::string& getLastName() const { return last_name_; }
+	const std::string& getFirstName() const { return first_name_; }
+	const std::string& getPatronymic() const { return patronymic_; }
+
+	void setLastName(const std::string& last_name)  { last_name_ = last_name; }	
+	void setFirstame(const std::string& first_name) { first_name_ = first_name; }
+	void setpatronymic(const std::string& patronymic) { patronymic_ = patronymic; }
+
 };
