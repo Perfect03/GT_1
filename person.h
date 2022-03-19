9650e417b1a1b698fc52028de8eb9509f8f0adab
Perfect_03 <zlobin_gg52926@mail.ru>
2022-03-19 12:26:59 +0600

    add "person.h" v1

---
+++ person.h
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <string>
+#include <functional>
+#include <list>
+ 
+using namespace std;
+
+class Person
+{
+private:
+	long key;
+	std::string last_name;
+	std::string first_name;
+public:
+	Person();
+	Person(const Person&);
+	const std::string& getLastName() const { return last_name; }
+	const std::string& getFirstName() const { return first_name; }
+	void setLastName(const std::string&);	
+	void setFirstame(const std::string&);
+};
